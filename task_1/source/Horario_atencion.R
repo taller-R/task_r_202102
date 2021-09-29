7a4008a2cbf3a7e824f0296ff84cce311c5006d6
eduard-martinez <edfemagonza@gmail.com>
2021-09-29 18:36:10 -0500

    hint task 1

--- task_1/source/Horario_atencion.R
+++ task_1/source/Horario_atencion.R
@@ -1,13 +1,13 @@
-#
-
 
 #
 rm(list=ls())
 require(pacman)
-p_load(tidyverse,rio)
+p_load(tidyverse,rio,skimr)
 
+#=========#
+# Punto 1 #
+#=========#
 
-# Punto 1
 1:100
 n_1_100 = seq(1,100,1)
 impares = seq(1,99,2)
@@ -33,20 +33,48 @@ a[c(T,F,F,F,F,T)]
 pares = n_1_100[!n_1_100  %in% impares]
 
 
+#=========#
+# Punto 2 #
+#=========#
+
+# importar datos
+data = import("task_1/data/input/cultivos.xlsx")
+
+# renombrar datos
+colnames(data) = data[4,] %>% tolower()
+
+# covertir codmpio en numerica
+data$codmpio = as.numeric(data$codmpio)
+
+# eliminar NA
+data = data %>% drop_na(codmpio)
+
+# convertir variables en numericas
+years = 1999:2007 %>% as.character()
+for(var in years){
+    data[,var] = as.numeric(data[,var])
+}
+
+# pivotear los datos
+pivot = data %>% pivot_longer(!coddepto:municipio,names_to="year",values_to="ha_cultivos")
+
+#=========#
+# Punto 3 #
+#=========#
+
 # punto 3
 cabe_cg = readRDS("task_1/data/input/2019/Cabecera - Caracteristicas generales (Personas).rds")
-cabe_res = readRDS("task_1/data/input/2019/Resto - Caracteristicas generales (Personas).rds")
+
+cabe_ocu = readRDS("task_1/data/input/2019/Cabecera - Ocupados.rds") %>% mutate(ocupado=1)
+
+cabe_des = readRDS("task_1/data/input/2019/Cabecera - Desocupados.rds") %>% mutate(desocupado=1)
+
+# unir datos
+geih = left_join(cabe_cg,cabe_ocu,c("secuencia_p","orden","directorio")) %>%
+        left_join(.,cabe_des,c("secuencia_p","orden","directorio")) 
+
+skim(geih)
 
 
 
-data = storms
-data$ts_diameter = ifelse(test = is.na(data$ts_diameter)==T, -1 ,  data$ts_diameter)
 
-data = data %>% mutate(hu_diameter = ifelse(test = is.na(hu_diameter)==T, -1 ,hu_diameter))
-
-SI(CONDICION,SI,NO)
-IFELSE(CONDICION,SI,NO)
-
-
-
-IF()
