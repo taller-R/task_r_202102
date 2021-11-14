b730d1683490f7894964a8f2c1d6ff2273d46b06
max197 <ma.gomez1@uniandes.edu.co>
2021-11-14 15:27:38 -0500

    src file added

---
+++ task_2/src/src.R
@@ -0,0 +1,16 @@
+## Elaborado por: Max Gómez
+# Colaboradores: 
+# Fecha de elaboracion: 14/11/2021
+
+#######################
+#---------Taller A
+
+#---------Punto 1
+#1.0 Crear lista 
+
+chip = vector(mode="list")
+
+
+#1.1 Importar datos
+
+num_files
